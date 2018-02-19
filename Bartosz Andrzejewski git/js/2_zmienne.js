b43daabf17e961ff461cfdcecd574aa4d653ddd1
andrzejewskiii <bartoszandrzejewski@poczta.onet.pl>
2018-02-19 13:05:09 +0100

    wszystko

---
+++ Bartosz Andrzejewski git/js/2_zmienne.js
@@ -0,0 +1,28 @@
+var imie;
+imie = "Janusz";
+var nazwisko = "Kowal";
+document.write("Imię: " + imie + "<br>");
+document.write("Nazwisko: ");
+document.write(nazwisko + "<br>");
+// + konkatenacja
+var wiek = 15;
+
+if(wiek>=18){
+    document.write("Jesteś pełnoletni<br>");
+} else if(wiek<15){
+    document.write("Jesteś dzieckiem<br>");
+} else {
+    document.write("Nie jesteś pełnoletni<br>");
+}
+
+var x, y;
+x = 9;
+y = 5;
+var suma = x + y;
+var roznica = x - y;
+var dzielenie;
+dzielenie = x / y;
+var mnozenie = x * y;
+var modulo = x % y;
+console.log(modulo);
+var
